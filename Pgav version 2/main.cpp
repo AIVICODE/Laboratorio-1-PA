e90e62065245bd12f60c7638d7bf289e351550fe
Thiago Cedrés <104656328+Cedrego@users.noreply.github.com>
2023-04-07 19:07:27 -0300

    Algunos ajustes y cambios
    - CheckIn y CheckOut ahora funcionan con tokens.
    - Booleanos pasan por un control antes de tomar el valor dado.
    - Diminutos cambios estéticos.
    - Posibilidad de checkear los datos ingresados antes de guardarlos con la opción de re ingresarlos.
    - Cualquier otra cosa que me olvide de mencionar 😅

--- Pgav version 2/main.cpp
+++ Pgav version 2/main.cpp
@@ -9,10 +9,11 @@ using namespace std;
 
 int main() {
     Systema systema;
-
+    bool loop = false;
     bool salir = false;
     while (!salir) {
-        cout << "Bienvenido al sistema de gestión de hostel" << endl;
+        cout << endl;
+        cout << "<< Bienvenido al sistema de gestión de hostel >>" << endl;
         cout << "1. Agregar Huesped" << endl;
         cout << "2. Agregar Habitación" << endl;
         cout << "3. Obtener Huéspedes" << endl;
@@ -22,23 +23,66 @@ int main() {
         cout << "7. Salir" << endl;
 
         int opcion;
-        cout << "Ingrese la opción deseada: ";
+        cout << "Ingrese la opción deseada > ";
         cin >> opcion;
+        cout << endl;
 
         switch (opcion) {
             case 1: {
-                cout << "Ingrese el nombre del huésped: ";
-                string nombre;
-                cin >> nombre;
+                
+                do{
+                    loop = false;
+                    
+                    cout << "Ingrese el nombre del huésped: ";
+                    string nombre;
+                    cin >> nombre;
 
-                cout << "Ingrese el email del huésped: ";
-                string email;
-                cin >> email;
+                    cout << "Ingrese el email del huésped: ";
+                    string email;
+                    cin >> email;
 
-                cout << "El huésped es tecnopacker? (1: si / 0: no): ";
-                bool esFinger;
-                cin >> esFinger;
-
+                    bool esFinger;
+                    do{
+                        loop = false;
+                        cout << "El huésped es tecnopacker? (1: si / 0: no): ";
+                        switch(input()){
+                            case 1:
+                                esFinger = 1;
+                                break;
+                            case 0:
+                                esFinger = 0;
+                                break;
+                            default:
+                                cout << "Error, valor no incluido en las opciones dadas." << endl;
+                                loop = true;
+                                break;
+                        }
+                    }while(loop == true);
+                    
+                    cout << endl;
+                    cout << "Huesped: " << nombre << endl;
+                    cout << "Email: " << email << endl;
+                    cout << "Tecnopacker: ";
+                    if(esFinger == 1){
+                        cout << "si" << endl;
+                    }
+                    else{
+                         cout << "no" << endl;
+                    }
+                    cout << "¿Estos datos son correctos? (1: si / 0: no) :";
+                    switch(input()){
+                            case 1:
+                                break;
+                            case 0:
+                                loop = true;
+                                break;
+                            default:
+                                cout << "Error al agregar huésped" << endl;
+                                loop = true;
+                                break;
+                        }
+                    
+                }while(loop == true);
                 try {
                     systema.agregarHuesped(nombre, email, esFinger);
                     cout << "Huésped agregado correctamente" << endl;
@@ -52,12 +96,31 @@ int main() {
             case 2: {
                 int numero, capacidad;
                 float precio;
-                cout << "Ingrese numero de habitacion: ";
-                cin >> numero;
-                cout << "Ingrese precio de la habitacion: ";
-                cin >> precio;
-                cout << "Ingrese capacidad de la habitacion: ";
-                cin >> capacidad;
+                do{
+                    loop = false;
+                    cout << "Ingrese numero de habitacion: ";
+                    cin >> numero;
+                    cout << "Ingrese precio de la habitacion: ";
+                    cin >> precio;
+                    cout << "Ingrese capacidad de la habitacion: ";
+                    cin >> capacidad;
+                    
+                    cout << "Nro de Habitación: " << numero << endl;
+                    cout << "Precio de Habitación: " << precio << endl;
+                    cout << "Capacidad de Habitación: " << capacidad << endl;
+                    cout << "¿Son estos datos correctos? (1: si / 0: no) :";
+                     switch(input()){
+                            case 1:
+                                break;
+                            case 0:
+                                loop = true;
+                                break;
+                            default:
+                                cout << "Error al agregar Habitación, Vuelva a ingresar los datos." << endl;
+                                loop = true;
+                                break;
+                        }
+               }while(loop == true);
                 try {
                     systema.agregarHabitacion(numero, precio, capacidad);
                     cout << "Habitacion agregada exitosamente." << endl;
@@ -105,36 +168,102 @@ int main() {
                 float precio;
                 int option;
                 bool pago;
-                cout << "Ingrese email: "<<endl;
-                cin >> mail;
+                do{
+                    loop=false;
+                    cout << "Ingrese email: "<<endl;
+                    cin >> mail;
 
-                cout << "Ingrese fecha CheckIn formato DD,MM,AAAA: "<<endl;
-                cin >> dia;
-                cin >> mes;
-                cin >> ano;
+                    do{
+                        loop = false;
+                        cout << "Ingrese fecha Check-In formato DD/MM/AAAA: "<<endl;
+                        cin >> ano;
+                        dia = strtok(ano,"-,/ \()");
+                        mes = strtok(ano,"-,/ \()");
+                        if(dia < 1 || dia > 31 || mes < 1 || mes > 12 || ano < 1500 || ano > 3500){
+                            loop = true;
+                            cout << "Error al ingresar la fecha de Check-In" << endl;
+                        }
+                    }while(loop == true);
 
-                cout << "Ingrese fecha CheckOut formato DD,MM,AAAA: "<<endl;
-                cin >> dia_;
-                cin >> mes_;
-                cin >> ano_;
+                    do{
+                        loop = false;
+                        cout << "Ingrese fecha Check-Out formato DD/MM/AAAA: "<<endl;
+                        cin >> ano_;
+                        dia_ = strtok(ano_,"-,/ \");
+                        mes_ = strtok(ano_,"-,/ \");
+                        if(dia_ < 1 || dia_ > 31 || mes_ < 1 || mes_ > 12 || ano_ < 1500 || ano_ > 3500){
+                            loop = true;
+                            cout << "Error al ingresar la fecha de Check-Out" << endl;
+                        }
+                    }while(loop == true);
 
-
-                cout<< "Ingrese numero de habitacion"<<endl;
-                cin >> habitacion;
-
-                cout << "Su habitacion es 1)individual o 2) grupal?"<<endl;
-                cin >> option;
-               if (option==1)
-               {
-                bool pago;
-                int cond;
-                cout<<"Pago la reserva? 1)si 2)no"<<endl;
-                cin >> cond;
-                if(cond==1){
-                    pago=true;
-                }else{
-                    pago=false;
-                }
+                    cout<< "Ingrese numero de habitacion"<<endl;
+                    cin >> habitacion;
+                    do{
+                        loop = false;
+                        cout << "Su habitacion es 1)individual o 2) grupal?"<<endl;
+                        cin >> option;
+                        switch(option){
+                            case 1:
+                            case 2:
+                                break;
+                            default:
+                                loop = true;
+                                cout << "Error, opcion ingresada no es valida" << endl;
+                                break;
+                        }
+                    }while(loop = true);
+                    
+                   if (option==1){
+                        bool pago;
+                        int cond;
+                        do{
+                            loop = false;
+                            cout << "Pago la reserva? 1)si 2)no" << endl;
+                            cin >> cond;
+                            switch(cond){
+                                case 1:
+                                    pago = true;
+                                    break;
+                                case 2:
+                                    pago = false;
+                                    break;
+                                default:
+                                    cout << "El valor ingresado no es valido, Vuelva a ingresar el dato" << endl;
+                                    loop = true;
+                            }
+                        }while(loop == true);
+                    }
+                    cout << "Email: " << mail << endl;
+                    cout << "Fecha de Check-In: " << dia << "/" << mes << "/" << ano << endl;
+                    cout << "Fecha de Check-Out: " << dia_ << "/" << mes_ << "/" << ano_ << endl;
+                    cout << "Nro de Habitación: " << habitacion << endl;
+                    cout << "Tipo: ";
+                    if(option == 1){
+                        cout << "Individual" << endl;
+                        if(cond == 1){
+                            cout << "Reserva pagada" << endl;
+                        }
+                        else{
+                            cout << "Reserva no pagada" << endl;
+                        }
+                    }
+                    else{
+                        cout << "Grupal" << endl;
+                    }
+                    cout << "¿Son estos datos correctos? (1: si / 0: no) :"
+                    switch(input()){
+                        case 1:
+                            break;
+                        case 0:
+                            loop = true;
+                            break;
+                        default:
+                            cout << "Error al registrar la reserva" << endl;
+                            loop = true;
+                            break;
+                    }  
+                }while(loop == true);
                 try {
                     systema.registrarReserva(mail, new DTReservaIndividual(codigo, DTFecha(dia,mes,ano), DTFecha(dia_,mes_,ano_), EstadoReserva(0), habitacion,pago));
                     cout << "Reserva agregada" << endl;
@@ -191,6 +320,7 @@ int main() {
                 break;
             }
         }
+    cout << endl;
     }
 
     return 0;
